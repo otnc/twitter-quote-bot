ce21dd9e9ff1e755e5ca9389360ab3a97a39ce82
otnc <pineapple2006.11.2@gmail.com>
2026-06-06 11:36:44 +0900

    chore: set up ESLint/Prettier tooling and build config
    ESLint flat config と typescript-eslint を追加し lint/lint:fix スクリプトを整備。tsdown を index/register の 2 エントリ構成に変更。data/ を gitignore に追加。

---
+++ eslint.config.js
@@ -0,0 +1,26 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import prettier from "eslint-config-prettier";
+
+export default tseslint.config(
+  {
+    ignores: ["dist/", "node_modules/", "data/"],
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+    },
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
+      ],
+    },
+  },
+  prettier
+);
